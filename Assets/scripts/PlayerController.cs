4af45b1c7f81df3037821a2cac6e3d40ed0c5715
Daniel Charpentier <d.y.charpen@gmail.com>
2017-01-25 00:02:00 -0500

    squaremobile!
    removed rotation from player model, added moving with arrow keys, and
    jumping but only when grounded.

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -6,12 +6,20 @@ public class PlayerController : MonoBehaviour {
 
     public Sprite playerSprite;
     SpriteRenderer sr;
-    PolygonCollider2D pc;
+    BoxCollider2D bc;
     Rigidbody2D rb;
+    float speed;
+    bool exist;
+    bool grounded;
+    Vector3 move;
+
+
 
     // Use this for initialization
     void Start() {
-
+        speed = 0.1f;
+        exist = false;
+        grounded = false;
     }
 
     // Update is called once per frame
@@ -21,14 +29,29 @@ public class PlayerController : MonoBehaviour {
 
     void FixedUpdate()
     {
+        if (exist)
+        {
+            move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+            transform.position += move * speed;
 
+            if (rb.velocity.y == 0) grounded = true;
+            else grounded = false;
+
+            if (Input.GetKeyDown("space") && grounded)
+            {
+                rb.AddForce(new Vector2(0, 500));
+            }
+        }
     }
 
     public void CreatePlayer()
     {
         sr = gameObject.AddComponent<SpriteRenderer>();
         sr.sprite = playerSprite;
-        pc = gameObject.AddComponent<PolygonCollider2D>();
+        bc = gameObject.AddComponent<BoxCollider2D>();
         rb = gameObject.AddComponent<Rigidbody2D>();
+
+        rb.freezeRotation = true;
+        exist = true;
     }
 }
