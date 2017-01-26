cdaf11ebdac3ecd2dff2435e3b3595d73648aad7
1a4blazeit <d.y.charpen@gmail.com>
2017-01-26 14:51:09 -0500

    made platforms get thinner as time goes by

--- Assets/scripts/LevelController.cs
+++ Assets/scripts/LevelController.cs
@@ -14,6 +14,7 @@ public class LevelController : MonoBehaviour {
 	int floorHeight; //track the height of the next floor
 	public int killPlane; //track the point at which the player should die, as they have no chance of reaching the bottom floor
 	int floorHoriz; //temp until random put in; determines next floor horizontal placement
+	float platformWidth; // make it a little tougher shall we?
 
 	// Use this for initialization
 	void Start () {
@@ -36,12 +37,15 @@ public class LevelController : MonoBehaviour {
 		
 		floorInstance[nextFloor] = Instantiate(floor) as GameObject;
 		floorInstance[nextFloor].GetComponent<Transform>().position = new Vector3(floorHoriz - 4, floorHeight, 0);
+		floorInstance[nextFloor].GetComponent<Transform>().localScale = new Vector3(platformWidth, 1, 1);
 		
 		floorHoriz = (floorHoriz + 4) % 12;
 		floorHeight = floorHeight + 3;
 		killPlane = killPlane + 3;
 		
 		nextFloor = (nextFloor + 1) % 5;
+		
+		platformWidth = platformWidth * 0.9f;
 		
 	}
 	
@@ -51,6 +55,7 @@ public class LevelController : MonoBehaviour {
 		floorHeight = -2;
 		killPlane = -5;
 		floorHoriz = 4;
+		platformWidth = 1.0f;
 		
 		for(int i = 0; i < 5; i++) {
 			floorInstance[i] = Instantiate(floor) as GameObject;
