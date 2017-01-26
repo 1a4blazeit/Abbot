032f3b242de451ad531a8b10a6aa7e6f05f19606
1a4blazeit <d.y.charpen@gmail.com>
2017-01-26 10:41:17 -0500

    Multiple platforms - made the floor object in mastercontroller an array to allow for multiple floors to be instantiated properly, and spawned 3 of them

--- Assets/scripts/MasterController.cs
+++ Assets/scripts/MasterController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MasterController : MonoBehaviour {
 
     public GameObject floor;
-	GameObject floorInstance;
+	GameObject[] floorInstance;
 	
 	
 
@@ -21,8 +21,17 @@ public class MasterController : MonoBehaviour {
             GameObject.Find("UIModel").GetComponent<UIController>().RemoveCenteredText();
             GameObject.Find("PlayerModel").GetComponent<PlayerController>().CreatePlayer();
 			
-			floorInstance = Instantiate(floor);
-			floorInstance.GetComponent<Transform>().position = new Vector3(0, -2, 0);
+			floorInstance = new GameObject[3];
+			
+			floorInstance[0] = Instantiate(floor) as GameObject;
+			floorInstance[0].GetComponent<Transform>().position = new Vector3(0, -2, 0);
+			
+			floorInstance[1] = Instantiate(floor) as GameObject;
+			floorInstance[1].GetComponent<Transform>().position = new Vector3(4, 1, 0);
+			
+			floorInstance[2] = Instantiate(floor) as GameObject;
+			floorInstance[2].GetComponent<Transform>().position = new Vector3(0, 4, 0);
+
         }
 	}
 
