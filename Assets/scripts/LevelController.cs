1ae2fae3da22008e616bfde23eb64550a74afa5a
1a4blazeit <d.y.charpen@gmail.com>
2017-01-26 11:08:21 -0500

    added level controller and moved functionality over. added check to stop player from hitting enter again to generate more level and fuck shit up

---
+++ Assets/scripts/LevelController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour {
+
+	public GameObject floor;
+	GameObject[] floorInstance;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void GenerateLevel () {
+		floorInstance = new GameObject[3];
+		
+		floorInstance[0] = Instantiate(floor) as GameObject;
+		floorInstance[0].GetComponent<Transform>().position = new Vector3(0, -2, 0);
+		
+		floorInstance[1] = Instantiate(floor) as GameObject;
+		floorInstance[1].GetComponent<Transform>().position = new Vector3(4, 1, 0);
+		
+		floorInstance[2] = Instantiate(floor) as GameObject;
+		floorInstance[2].GetComponent<Transform>().position = new Vector3(0, 4, 0);
+	}
+}
--- Assets/scripts/MasterController.cs
+++ Assets/scripts/MasterController.cs
@@ -3,34 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MasterController : MonoBehaviour {
-
-    public GameObject floor;
-	GameObject[] floorInstance;
 	
-	
+	bool begun;
 
 	// Use this for initialization
 	void Start () {
+		begun = false;
         GameObject.Find("UIModel").GetComponent<UIController>().PrintCenteredText("Press Enter!");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown("return"))
+		if (!begun && Input.GetKeyDown("return"))
         {
             GameObject.Find("UIModel").GetComponent<UIController>().RemoveCenteredText();
             GameObject.Find("PlayerModel").GetComponent<PlayerController>().CreatePlayer();
-			
-			floorInstance = new GameObject[3];
-			
-			floorInstance[0] = Instantiate(floor) as GameObject;
-			floorInstance[0].GetComponent<Transform>().position = new Vector3(0, -2, 0);
-			
-			floorInstance[1] = Instantiate(floor) as GameObject;
-			floorInstance[1].GetComponent<Transform>().position = new Vector3(4, 1, 0);
-			
-			floorInstance[2] = Instantiate(floor) as GameObject;
-			floorInstance[2].GetComponent<Transform>().position = new Vector3(0, 4, 0);
+			GameObject.Find("LevelModel").GetComponent<LevelController>().GenerateLevel();
+			begun = true;
 
         }
 	}
