5e6b49f72b4133f3866f306c7f3c8b8cbce80e37
1a4blazeit <d.y.charpen@gmail.com>
2017-01-27 16:49:43 -0500

    exit portal added to end of level, triggers on touch to reset level with happy message!

---
+++ Assets/ExitScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter2D (Collider2D other) {
+		if(other.gameObject.tag == "Player")
+			GameObject.Find("MasterModel").GetComponent<MasterController>().FinishedLevel();
+	}
+
+}
--- Assets/scripts/MasterController.cs
+++ Assets/scripts/MasterController.cs
@@ -59,5 +59,10 @@ public class MasterController : MonoBehaviour {
 		
 		
 	}
+	
+	public void FinishedLevel() {
+		startMessage = "You did it!";
+		OnReset();
+	}
 
 }
